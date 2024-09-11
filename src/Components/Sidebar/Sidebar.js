ad6959285ece068b01d405db3053fd03d6d4ec08
alireza <afshar2451382@gmail.com>
2024-09-11 13:31:44 +0330

    sidebar-icon

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { BiCommentDetail } from "react-icons/bi";
+import { FiUsers } from "react-icons/fi";
+import { BsBagCheck, BsCurrencyDollar } from "react-icons/bs";
+
+import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
@@ -7,22 +14,40 @@ export default function Sidebar() {
 
       <ul className="sidebar-links">
         <li>
-          <a href="#">صفحه اصلی</a>
+          <a href="#">
+            <AiOutlineHome className="icon" />
+            صفحه اصلی
+          </a>
+        </li>
+        <li className="active">
+          <a href="#">
+            <MdProductionQuantityLimits className="icon" />
+            محصولات
+          </a>
         </li>
         <li>
-          <a href="#">محصولات</a>
+          <a href="#">
+            <BiCommentDetail className="icon" />
+            کامنت ها
+          </a>
         </li>
         <li>
-          <a href="#">کامنت ها</a>
+          <a href="#">
+            <FiUsers className="icon" />
+            کاربران
+          </a>
         </li>
         <li>
-          <a href="#">کاربران</a>
+          <a href="#">
+            <BsBagCheck className="icon" />
+            سفارشات
+          </a>
         </li>
         <li>
-          <a href="#">سفارشات</a>
-        </li>
-        <li>
-          <a href="#">تخفیف ها</a>
+          <a href="#">
+            <BsCurrencyDollar className="icon" />
+            تخفیف ها
+          </a>
         </li>
       </ul>
     </div>
