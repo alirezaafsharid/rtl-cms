ed0bb54fa1b2b2d929b46f1956dfca65b1288d53
alireza <afshar2451382@gmail.com>
2024-09-11 17:49:58 +0330

    c-header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-// import logo from './logo.svg';
+import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
 import "./App.css";
 
 function App() {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+        {/* Router */}
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { AiOutlineBell } from "react-icons/ai";
+import { BsBrightnessHigh } from "react-icons/bs";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="admin-profile">
+        <img
+          src="/rtl-cms/public/img/modle-mo-kelasik-madane-2.jpg"
+          alt="Admin Profile"
+        />
+        <div>
+          <h1>علیرضا افشار</h1>
+          <h3>برنامه نویس فرانت اند</h3>
+        </div>
+      </div>
+
+      <div className="header-left-section">
+        <div className="search-box">
+          <input type="text" placeholder="جست و جو بکنید ..." />
+          <button>جست و جو</button>
+        </div>
+
+        <button className="header-left-icon">
+          <AiOutlineBell />
+        </button>
+        <button className="header-left-icon">
+          <BsBrightnessHigh />
+        </button>
+      </div>
+    </div>
+  );
+}
