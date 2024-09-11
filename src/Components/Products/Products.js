5cad18792935caf672e8d8226fe1ffc24ba27aa8
alireza <afshar2451382@gmail.com>
2024-09-11 23:32:15 +0330

    product-table

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Errorbax from "../Errorbax/Errorbax";
 import AddNewProduct from "../AddNewProduct/AddNewProduct";
+import ProductsTable from "../ProductsTable/ProductsTable";
 
 export default function Products() {
   return (
     <>
       <AddNewProduct />
-      <Errorbax msg="محصولی یافت نشد" />;
+      <Errorbax msg="محصولی یافت نشد" />
+      <ProductsTable />
     </>
   );
 }
