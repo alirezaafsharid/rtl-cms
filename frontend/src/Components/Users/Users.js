02b7ef3b06e684843f7ebe6e0fb53cf4ccb06a26
alireza <afshar2451382@gmail.com>
2024-09-19 12:31:55 +0330

    ویرایش اطلاعات کاربران رف رو سرور

--- frontend/src/Components/Users/Users.js
+++ frontend/src/Components/Users/Users.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import DeleteModal from "../DeleteModal/DeleteModal";
-// import EditeModal from "../EditModal";
+import EditModal from "../EditModal/EditModal";
 // import DetailsModal from "../DetailsModal";
+import { AiOutlineDollarCircle } from "react-icons/ai";
 import ErrorBox from "../Errorbox/Errorbox";
+import "./Users.css";
 
+// ----------------------------------------------------------------------------------------
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [allUsers, setAllUsers] = useState([]);
@@ -11,9 +14,21 @@ export default function Users() {
   const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
   const [isShowEditModal, setIsShowEditModal] = useState(false);
   const [usersID, setUsersID] = useState(null);
-  // ------------------------------------------------------------------------------------------------
 
+  const [userNewFirstname, setUserNewFirstname] = useState("");
+  const [userNewLastname, setUserNewLastname] = useState("");
+  const [userNewUsername, setUserNewUsername] = useState("");
+  const [userNewPassword, setUserNewPassword] = useState("");
+  const [userNewPhone, setUserNewPhone] = useState("");
+  // const [userNewCity, setUserNewCity] = useState('')
+  const [userNewEmail, setUserNewEmail] = useState("");
+  // const [userNewAddress, setUserNewAddress] = useState('')
+  // const [userNewBuy, setUserNewBuy] = useState('')
+  // const [userNewScore, setUserNewScore] = useState('')
+
+  // ------------------------------------------------------------------------------------------------
   const closeDeleteModal = () => setIsShowDeleteModal(false);
+  const closeEditModal = () => setIsShowEditModal(false);
   // ------------------------------------------------------------------------------------------------
   function getAllUsers() {
     fetch("https://dummyjson.com/users")
@@ -39,6 +54,29 @@ export default function Users() {
         getAllUsers();
       });
     console.log("کامنت با موفقیت ریمو شد");
+  };
+  // ----------------------------------------------------------------------------------------------------
+  const updateUsers = (event) => {
+    event.preventDefault();
+    const userNewInfos = {
+      firstName: userNewFirstname,
+      lastName: userNewLastname,
+      username: userNewUsername,
+      password: userNewPassword,
+      phone: userNewPhone,
+      email: userNewEmail,
+    };
+    /* updating lastName of user with id 2 */
+    fetch(`https://dummyjson.com/users/${usersID}`, {
+      method: "PUT" /* or PATCH */,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userNewInfos),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+    console.log("اطلاعات کاربر ویرایش شد");
+    setIsShowEditModal(false);
+    getAllUsers();
   };
   // ----------------------------------------------------------------------------------------------------
   return (
@@ -78,7 +116,20 @@ export default function Users() {
                       حذف
                     </button>
                     <button>جزییات</button>
-                    <button>ویرایش</button>
+                    <button
+                      onClick={() => {
+                        setIsShowEditModal(true);
+                        setUsersID(users.id);
+                        setUserNewFirstname(users.firstName);
+                        setUserNewLastname(users.lastName);
+                        setUserNewUsername(users.username);
+                        setUserNewPassword(users.password);
+                        setUserNewPhone(users.phone);
+                        setUserNewEmail(users.email);
+                      }}
+                    >
+                      ویرایش
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -94,6 +145,82 @@ export default function Users() {
             onHide={closeDeleteModal}
             submitAction={deleteUser}
           />
+        )}
+        {isShowEditModal && (
+          <EditModal onClose={closeEditModal} onSubmit={updateUsers}>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewFirstname}
+                onChange={(event) => setUserNewFirstname(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewLastname}
+                onChange={(event) => setUserNewLastname(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewUsername}
+                onChange={(event) => setUserNewUsername(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewPassword}
+                onChange={(event) => setUserNewPassword(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewPhone}
+                onChange={(event) => setUserNewPhone(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+            <div className="edit-user-info-input-group">
+              <span>
+                <AiOutlineDollarCircle />
+              </span>
+              <input
+                type="text"
+                className="edit-user-info-input"
+                value={userNewEmail}
+                onChange={(event) => setUserNewEmail(event.target.value)}
+                placeholder="مقدار جدید را وارد نمایید"
+              />
+            </div>
+          </EditModal>
         )}
       </div>
     </>
